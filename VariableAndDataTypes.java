1febbddea228af6e38df9edd44e105219c71bf2b
anudeep2392 <anudeepsanka108@gmail.com>
2026-01-04 17:00:21 -0600

    Add files via upload

---
+++ VariableAndDataTypes.java
@@ -0,0 +1,14 @@
+public class VariableAndDataTypes {
+
+    public static void main (String[] args){
+
+        int age = 32;
+        double height = 5.6;
+        String name = "Anudeep";
+
+        System.out.println("Name: " + name + ",Age: " + age + ",Height: " + height);
+
+
+    }
+
+}
